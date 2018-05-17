22210f573485514222c2daefd438c11448e39ab8
Philipp Muens <raute1337@gmx.de>
2018-05-17 21:05:40 +0200

    Add checks for processor support

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -4,6 +4,11 @@ extern kmain
 section .text
 bits 32
 start:
+    mov esp, stack_top
+    ; check if processor supports necessary features
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
     ; point first p4 entry to first p3 entry
     mov eax, p3_table
     or eax, 0b11
@@ -50,6 +55,69 @@ start:
     jmp gdt64.code:kmain
     hlt
 
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+.no_multiboot:
+    mov al, "0"
+    jmp error
+
+check_cpuid:
+    ; check if CPUID is supported by attempting to flip the ID bit (bit 21)
+    ; in the FLAGS register. If we can flip it, CPUID is available.
+    ; copy FLAGS in to EAX via stack
+    pushfd
+    pop eax
+    ; copy to ECX as well for comparing later on
+    mov ecx, eax
+    ; flip the ID bit
+    xor eax, 1 << 21
+    ; copy EAX to FLAGS via the stack
+    push eax
+    popfd
+    ; copy FLAGS back to EAX (with the flipped bit if CPUID is supported)
+    pushfd
+    pop eax
+    ; restore FLAGS from the old version stored in ECX (i.e. flipping the
+    ; ID bit back if it was ever flipped).
+    push ecx
+    popfd
+    ; compare EAX and ECX. If they are equal then that means the bit
+    ; wasn't flipped, and CPUID isn't supported.
+    cmp eax, ecx
+    je .no_cpuid
+    ret
+.no_cpuid:
+    mov al, "1"
+    jmp error
+
+check_long_mode:
+    ; test if extended processor info in available
+    mov eax, 0x80000000        ; implicit argument for cpuid
+    cpuid                      ; get highest supported argument
+    cmp eax, 0x80000001        ; it needs to be at least 0x80000001
+    jb .no_long_mode           ; if it's less, the CPU is too old for long mode
+
+    ; use extended info to test if long mode is available
+    mov eax, 0x80000001        ; argument for extended processor info
+    cpuid                      ; returns various feature bits in ecx and edx
+    test edx, 1 << 29          ; test if the LM-bit is set in the D-register
+    jz .no_long_mode           ; If it's not set, there is no long mode
+    ret
+.no_long_mode:
+    mov al, "2"
+    jmp error
+
+; prints `ERR: ` and the given error code to screen and hangs.
+; parameter: error code (in ascii) in al
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+    hlt
+
 section .bss
 align 4096
 p4_table:
@@ -58,6 +126,9 @@ p3_table:
     resb 4096
 p2_table:
     resb 4096
+stack_bottom:
+    resb 64
+stack_top:
 
 section .rodata
 gdt64:
